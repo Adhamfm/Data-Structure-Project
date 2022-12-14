708757a80a5611ce5d3f20bf6280864ecc83f6a2
Mustafa Sayed Al-Said <101906607+MustafaSayedAl-Said@users.noreply.github.com>
2022-12-15 00:30:13 +0200

    Update Minify.cpp

--- src/Minify.cpp
+++ src/Minify.cpp
@@ -1,12 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<time.h>
+#include<stdlib.h>
+using namespace std;
 
-#include"minify.h"
-
-string minify(string in) {
-    string out = "";
-    for (int i = 0; i < in.size(); i++) {
-        if (in.at(i) != '\n' && in.at(i) != ' ' && in.at(i) != '\t') {
-            out += in.at(i);
+void minify(string inputFile, string outputFile) {
+    ifstream indata(inputFile);
+    ofstream outdata(outputFile);
+    string sentence;
+    while (getline(indata, sentence)) {		//Reading file
+        string res = "";
+        for (int i = 0; i < sentence.size(); i++) {
+            if (sentence.at(i) != '\t' && sentence.at(i) != '\n' && sentence.at(i) != ' ')
+                res += sentence.at(i);
         }
+        outdata << res;
     }
-    return out;
+    indata.close();
 }
