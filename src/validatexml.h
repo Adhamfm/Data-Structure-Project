17a016959789d3ed61a4ca962da5c0de29fd5d17
kaream1adel <96724633+kaream1adel@users.noreply.github.com>
2023-02-01 15:34:31 +0200

    Update validatexml.h

--- src/validatexml.h
+++ src/validatexml.h
@@ -9,18 +9,41 @@
 #define VALIDATEXML_H_
 
 #include <string>
+#include <fstream>
 
 
 using namespace std;
+
+#define missing_close 0
+#define missing_open  1
+#define Error_found	  0
+#define No_Error	  1
+
+
+
+
+
+
 
 
 class validatexml {
 
 
 public:
+
+	int  line_number;
+	string  tag;
+	int  status;
+	fstream read_file;
+	ofstream write_file;
+	int file_status=Error_found;
+
 	validatexml();
 
+
 	void check_xml(string string);
+
+	void correct_xml(string input_file,string output_file);
 
 
 
