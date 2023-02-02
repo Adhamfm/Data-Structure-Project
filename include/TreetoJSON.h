0b22baf7db1d180d1cb25b2bd7e2de0529eaaffe
Adhamfm <57398840+Adhamfm@users.noreply.github.com>
2023-02-02 20:20:57 +0200

    Update TreetoJSON.h

--- include/TreetoJSON.h
+++ include/TreetoJSON.h
@@ -10,11 +10,19 @@ class TreetoJSON
         std::string json="";
 
     public:
+        std::string influencer="";
+        std::string active="";
+        std::string currentname="";
+        int posts=0;
+        int followers=0;
         void dfs(NodeTree *node);
         void treeToJson(NodeTree *node);
         bool isLeaf(NodeTree *node);
         std::string getJSON();
         NodeTree convertxml(std::string xml);
+        void BFS(NodeTree *root);
+        void mostFollower(NodeTree *node);
+        void mostActive(NodeTree *node);
 };
 
 #endif // TREETOJSON_H
